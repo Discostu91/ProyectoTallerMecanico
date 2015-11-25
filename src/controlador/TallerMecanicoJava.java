ca84138af28fa6a84984ddfe605faef855dc9caf
Discostu91 <Alvaro@DESKTOP-C05Q6NR>
2015-11-25 00:45:27 -0300

    Inicio del proyecto, con subidas exclusivas desde el netbeans

---
+++ src/controlador/TallerMecanicoJava.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class TallerMecanicoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Este es el comienzo...");
+        System.out.println("del exito");
+         System.out.println("vamos  que se puede");
+         System.out.println("ready");
+         System.out.println("funciona la wea o no locooo???");
+         System.out.println("yyyyyyyy.....");
+         System.out.println("si funciona la wea ");
+         System.out.println(" ready");
+         System.out.println("yes");
+         System.out.println("bkn :D");
+
+    }
+    
+}
