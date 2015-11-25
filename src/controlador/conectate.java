a2f10d55c7dcc420ad4d92f964563f90bfce3ae4
Discostu91 <Alvaro@DESKTOP-C05Q6NR>
2015-11-25 20:17:40 -0300

    Se arreglaron problemas de sintaxis en la base de datos.

--- src/controlador/conectate.java
+++ src/controlador/conectate.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class conectate {
    static String bd = "tallermecanico";//nombre de la base de datos que esta en mysql
    static String login = "root";// usuario
-   static String password = "alvaro12345";// contraseña
+   static String password = "roottaller";// contraseña
    static String url = "jdbc:derby://localhost:1527/"+bd;
 
    Connection conn = null;
